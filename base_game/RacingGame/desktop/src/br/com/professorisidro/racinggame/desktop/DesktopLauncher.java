9394096b13069c51e6c3a421422e3b790ff6abbe
Mauro Mascarenhas <mauro.mascarenhas@nintersoft.com>
2019-04-18 17:20:29 -0300

    Added racing base game
    Added another base game (Racing) by @professorisidro

---
+++ base_game/RacingGame/desktop/src/br/com/professorisidro/racinggame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.com.professorisidro.racinggame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.com.professorisidro.racinggame.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
